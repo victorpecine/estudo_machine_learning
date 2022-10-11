74e882a4fdbaea00c5c6e65bc964a2ed784b218a
Victor Pecine <73555379+victorpecine@users.noreply.github.com>
2022-10-11 18:18:16 -0300

    modelo linear de previsao

--- precos_carros.py
+++ precos_carros.py
@@ -1,5 +1,10 @@
+from statistics import mode
 import pandas as pd
 from datetime import datetime
+from sklearn.model_selection import train_test_split
+from sklearn.svm import LinearSVC
+from sklearn.metrics import accuracy_score
+import numpy as np
 
 
 uri = "https://gist.githubusercontent.com/guilhermesilveira/4d1d4a16ccbf6ea4e0a64a38a24ec884/raw/afd05cb0c796d18f3f5a6537053ded308ba94bf7/car-prices.csv"
@@ -26,3 +31,24 @@ df_carros['km_por_ano'] = df_carros['milhas_por_ano'] * 1.60934
 
 
 df_carros = df_carros.drop(columns=['Unnamed: 0', 'milhas_por_ano'], axis=1)
+
+
+x = df_carros[['preco', 'idade_do_modelo', 'km_por_ano']]
+
+y = df_carros['vendido']
+
+
+# Modelo linear
+seed = 54
+
+np.random.seed(seed)
+
+treino_x, teste_x, treino_y, teste_y = train_test_split(x, y, test_size=0.25, stratify=y)
+
+modelo_linear = LinearSVC()
+modelo_linear.fit(treino_x, treino_y)
+
+previsoes = modelo_linear.predict(teste_x)
+
+precisao = accuracy_score(teste_y, previsoes)
+# 0.5864
