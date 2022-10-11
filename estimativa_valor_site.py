2fac76cf83c02f0b81f5a983db7c3416a8947cca
Victor Pecine <73555379+victorpecine@users.noreply.github.com>
2022-10-11 14:12:15 -0300

    modelo de previsao e teste de precisao

--- estimativa_valor_site.py
+++ estimativa_valor_site.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.svm import LinearSVC
+from sklearn.metrics import accuracy_score
+import numpy as np
 
 
 uri = "https://gist.githubusercontent.com/guilhermesilveira/1b7d5475863c15f484ac495bd70975cf/raw/16aff7a0aee67e7c100a2a48b676a2d2d142f646/projects.csv"
@@ -14,12 +18,29 @@ troca_valores = {0: 1, 1: 0}
 df_sites.insert(1, 'finalizado', df_sites['nao_finalizado'].map(troca_valores))
 
 
-# Análise gráfica
-ax = sns.scatterplot(data=df_sites, x='horas_esperadas', y='preco', hue='finalizado').get_figure()
+# Modelagem
+x = df_sites[['horas_esperadas', 'preco']]
 
-ax.savefig('graficos/horas_preco.png')
+y = df_sites['finalizado']
+
+treino_x, teste_x, treino_y, teste_y = train_test_split(x, y, random_state=131, test_size=0.25, stratify=y)
+
+modelo = LinearSVC()
+
+modelo.fit(treino_x, treino_y)
+
+previsoes = modelo.predict(teste_x)
+
+precisao = accuracy_score(teste_y, previsoes)
+# 0.5370370370370371
 
 
-ax_1 = sns.relplot(data=df_sites, x='horas_esperadas', y='preco', col='finalizado', hue='finalizado')
+# Previsão de base considerando todos os projetos como finalizados
+previsao_de_base = np.ones(540)
 
-ax_1.savefig('graficos/horas_preco_colunas.png')
+precisao_de_base = accuracy_score(teste_y, previsao_de_base)
+# 0.5259259259259259
+
+
+# O modelo estimado tem uma precisao 2,11% maior que a previsão de base
+# É necessário aumentar a precisão do modelo
