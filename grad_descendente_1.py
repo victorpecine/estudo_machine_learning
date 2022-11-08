ffb361d349764b63553a4456152f0e124d0f2051
CIC-039 <victorpecine@gmail.com>
2022-11-08 08:45:25 -0300

    definicao do modelo, funcao de custo e otimizador

--- grad_descendente_1.py
+++ grad_descendente_1.py
@@ -19,5 +19,17 @@ y = torch.from_numpy(y_numpy.astype(np.float32))
 # Ao invés de ficar na horizontal, ficará na vertical (cada campo é um registro)
 y = y.view(y.shape[0], 1)
 
-plt.plot(x_numpy, y_numpy, 'ro')
-plt.show()
+# plt.plot(x_numpy, y_numpy, 'ro')
+# plt.show()
+
+
+# Definição do modelo
+input_size = 1 # Apenas uma variável dependente
+output_size = 1
+model = nn.Linear(input_size, output_size)
+
+
+# Definição da função de custo e otimizador
+learning_rate = 0.01
+criterion = nn.MSELoss() # Método do erro quadrático médio (função de custo)
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
